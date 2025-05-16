336bd7be9efae78077e732001f3a92eb301783ad
andres98465120 <andres.orozcoq@est.iudigital.edu.co>
2025-05-16 17:24:01 -0500

    Update ConnectionUtil.java

--- ConnectionUtil.java
+++ ConnectionUtil.java
@@ -11,10 +11,10 @@ public class ConnectionUtil {
     private static final String USER = "root";
     private static final String PASSWORD = "Dener123";
 
-    // Función para obtener la conexión a la base de datos
+    
     public static Connection getConnection() throws SQLException {
         try {
-            // Asegúrate de que el driver esté cargado
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
@@ -23,17 +23,17 @@ public class ConnectionUtil {
         }
     }
 
-    // Función para obtener los detalles de la conexión a la base de datos
+    
     public static String getDatabaseDetails(Connection connection) throws SQLException {
         DatabaseMetaData metaData = connection.getMetaData();
         String databaseName = metaData.getDatabaseProductName();
         String databaseVersion = metaData.getDatabaseProductVersion();
 
-        // Obtener el nombre de la base de datos
+       
         String databaseURL = connection.getMetaData().getURL();
         String dbName = databaseURL.substring(databaseURL.lastIndexOf("/") + 1);
 
-        // Construir un mensaje con la información
+       
         return String.format("Conexión exitosa a la base de datos: %s\nVersión de la base de datos: %s\nBase de datos a la que estás conectado: %s",
                 databaseName, databaseVersion, dbName);
     }
